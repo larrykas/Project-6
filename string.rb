cd33720047e2aa1a3486f2349601e488f5969678
larrykas <larrykas@users.noreply.github.com>
2013-10-09 21:15:26 +0300

    Create string.rb
    converting strings to lowercase, Arrays and replacing characters in a string.

---
+++ string.rb
@@ -0,0 +1,13 @@
+
+# Assigns the variable greeting the string "HAPPY BIRTHDAY LARRY"
+greeting = "HAPPY BIRTHDAY LARRY"
+# Converts the String from uppercase to lowercase
+puts greeting.downcase
+# Assigns the variable song the string "Mary had a little lamb"
+song = "Mary had a little lamb"
+# Replaces all the a’s in this sentence with x’s
+puts song.gsub!('a', 'x')
+# The following two Convert the String "Bob" into an Array of Characters
+ p 'Bob'.chars.to_a
+ 
+ p 'Bob'.split(//)
